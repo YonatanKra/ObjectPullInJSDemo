448fa1ff3902b1f9b585e822935eeaa4398d307e
Yonatan Kra <yonatan.kr@walkme.com>
2019-12-07 11:29:07 +0200

    Object pool

--- objectPool.js
+++ objectPool.js
@@ -8,24 +8,104 @@ class Demo {
     }
 }
 
+
 function createAndDestroy() {
     function test() {
-        const objs = [];
         const counter = {
             count: 0
         }
         
+        const pool = new Pool(() => new Demo(counter),
+            (item) => {},
+            1000);
+
         for (let i = 0; i < 1000; i++) {
-            for (let boom = 0; boom < 1000; boom++) {
-                const currBoom = objs[objs.push(new Demo(counter)) - 1];
-                currBoom.setMe();
+            for (let demo = 0; demo < 1000; demo++) {
+                const currDemo = pool.getFree();
+                currDemo.data.setMe();
             }
+            // release all booms
+            pool.releaseAll();
         }
 
         console.log(`Counter is: ${counter.count}`);
     }
 
     repeat(test, 10, 'Create and Destroy');
+}
+
+class PoolObject {
+    constructor(data) {
+        this.data = data;
+        this.nextFree = null;
+        this.previousFree = null;
+    }
+}
+
+class Pool {
+    constructor(objCreator, objReseter, initialSize = 5000) {
+        this._pool = [];
+        this.objCreator = objCreator;
+        this.objReseter = objReseter;
+        for (let i = 0; i < initialSize; i++) {
+            this.addNewObject(this.newPoolObject());
+        }
+    }
+
+    addNewObject(obj) {
+        this._pool.push(obj);
+        this.release(obj);
+        return obj;
+    }
+
+    release(poolObject) {
+        // flag as free
+        poolObject.free = true;
+
+        // set in the dequeue
+        poolObject.nextFree = null;
+        poolObject.previousFree = this.lastFree;
+
+        // if we had a last free, set the last free's next as the new poolObject
+        // otherwise, this is the first free!
+        if (poolObject.previousFree) {
+            this.lastFree.nextFree = poolObject;
+        } else {
+            this.nextFree = poolObject;
+        }
+
+        // set the new object as the last in the dequeue
+        this.lastFree = poolObject;
+
+        // reset the object if needed
+        this.objReseter(poolObject);
+    }
+
+    getFree() {
+        // if we have a free one, get it - otherwise create it
+        const freeObject = this.nextFree ? this.nextFree : this.addNewObject(this.newPoolObject());
+
+        // flag as used
+        freeObject.free = false;
+
+        // the next free is the object's next free
+        this.nextFree = freeObject.nextFree;
+
+        // if there's nothing afterwards, the lastFree is null as well
+        if (!this.nextFree) this.lastFree = null;
+
+        // return the now not free object
+        return freeObject;
+    }
+
+    newPoolObject() {
+        const data = this.objCreator();
+        return new PoolObject(data, this.lastFree, this.nextFree);
+    }
+
+    releaseAll() {
+        this._pool.forEach(item => this.release(item));
+    }
 }
 
 function repeat(cb, repeats, testName) {
